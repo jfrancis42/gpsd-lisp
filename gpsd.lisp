ef5a9b7979448ead72e23d35789db51499946cb7
Jeff Francis <jeff@gritch.org>
2016-09-07 11:13:54 -0700

    Added comments and conversion functions.

--- gpsd.lisp
+++ gpsd.lisp
@@ -6,14 +6,39 @@
 (defparameter *uptodate-thread* nil)
 (defvar *location* nil)
 
+;; meters to feet
+(defmacro m-to-ft (m) `(* ,m 3.28084))
+
+;; meters per second to feet per second
+(defmacro ms-to-fs (m) `(* ,m 3.28084))
+
+;; meters per second to feet per minute
+(defmacro ms-to-fm (m) `(* ,m 196.85))
+
+;; meters per second to kilometers per hour
+(defmacro ms-to-kph (m) `(* ,m 3.6))
+
+;; meters per second to miles per hour
+(defmacro ms-to-mph (m) `(* ,m 2.23694))
+
+;; meters per second to nautical miles per hour
+(defmacro ms-to-kts (m) `(* ,m 1.94384))
+
 (defun socket-read (socket)
+  "Read a line of data from a socket (blocking)."
   (read-line (usocket:socket-stream socket)))
 
 (defun socket-print (string socket)
+  "Write data to a socket and flush."
   (write-line string (usocket:socket-stream socket))
   (force-output (usocket:socket-stream socket)))
 
 (defun watch-gpsd (&optional (iterations 10) (host "gpsd") (port 2947))
+  "Watch gpsd and display a new point every time the location is
+updated for a specified number of iterations. Defaults to a gpsd
+server named 'gpsd' and TCP port 2947. If you're running the gpsd on
+the same machine this code is running on, the address should most
+likely be '127.0.0.1'."
   (let ((socket (usocket:socket-connect host port))
 	(loc nil) (json nil) (sats nil))
     (socket-print "?WATCH={\"enable\":true,\"json\":true}" socket)
@@ -48,6 +73,8 @@
     ))
 
 (defun up-to-dater (host port)
+  "A thread that watches gpsd and keeps the internal data structures
+updated."
   (let ((socket (usocket:socket-connect host port))
 	(loc nil) (json nil) (sats nil))
     (socket-print "?WATCH={\"enable\":true,\"json\":true}" socket)
@@ -60,6 +87,7 @@
 	     ((equal "TPV" (cdr (assoc :class json)))
 	      (bt:with-lock-held (*gpsd-lock*)
 		(setf *location* (make-instance 'gps-point
+						:creation-source point-gps
 						:mode (cdr (assoc :mode json))
 						:sats sats
 						:lat (cdr (assoc :lat json))
@@ -69,7 +97,14 @@
 						:crs (cdr (assoc :track json)))))))))))
 
 (defun start-gpsd (&optional (host "gpsd") (port 2947))
+  "Start a thread that watches gpsd and constant updates the private
+local variable *location*.  Defaults to a gpsd server named 'gpsd' and
+TCP port 2947. If you're running the gpsd on the same machine this
+code is running on, the address should most likely be
+'127.0.0.1'."
   (setf *uptodate-thread* (bt:make-thread (lambda () (up-to-dater host port)) :name "gpsd"))
   (format t "gpsd update thread is running...~%"))
 
-;; (bt:with-lock-held (gpsd:*gpsd-lock*) (gpsd:pp gpsd::*location*))
+(defun get-current-location ()
+  "The approved method of obtaining the current location."
+  (bt:with-lock-held (gpsd:*gpsd-lock*) gpsd::*location*))
--- package.lisp
+++ package.lisp
@@ -10,10 +10,26 @@
 		:point-alt
 		:point-spd
 		:point-crs
+		:creation-time
 		:creation-source
+		:deg-to-cardinal-course
 		:point-gps)
   (:export :watch-gpsd
 	   :start-gpsd
-	   :*location*
-	   :*gpsd-lock*
-	   :pp))
+	   :get-current-location
+	   :pp
+	   :point-lat
+	   :point-lon
+	   :point-alt
+	   :point-spd
+	   :point-crs
+	   :point-creation-time
+	   :m-to-ft
+	   :ms-to-fs
+	   :ms-to-fm
+	   :ms-to-kph
+	   :ms-to-kts
+	   :ms-to-mph
+	   :deg-to-cardinal-course
+	   :true-to-magnetic
+	   :magnetic-to-true))
